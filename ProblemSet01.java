8957008a44521ff03a4e053d0041402f0de03686
ianframe <ian.frame@hies.org>
2017-09-02 12:03:16 -0400

    initial upload

---
+++ ProblemSet01.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.util.Scanner;
+import java.lang.Exception;
+
+public class ProblemSet01
+{
+    public static void america()
+    {
+        //create a Scanner for the input file 
+        File f = new File("./input/usa.txt");
+
+        //parse the initial data to obtain the canvas coordinates
+
+        //scale the width of the canvas. keep the default height of 512, and adjust the width proportionally
+
+        //adjust the coordinate system
+
+        //for speed of application wait to display the image until the program is complete. for cool effect, disable doubleBuffering
+        StdDraw.enableDoubleBuffering();
+
+        //there isn't an integer to tell us how many lines of code exist,
+        //so you should first check if there is still a line in existence
+
+    }
+
+    public static void web(int n, double p)
+    {
+        
+    }
+
+    public static void checkerboard(int n)
+    {
+        
+    }
+    
+    public static void rose(int n)
+    {
+        
+    }
+}
